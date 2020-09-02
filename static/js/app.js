174782038be5bda71b8c59a3a4e4f3fd57bfbca4
Tameka Kuar <tamekakuar17@gmail.com>
2020-09-02 10:56:33 -0400

    update code

---
+++ static/js/app.js
@@ -0,0 +1,19 @@
+// Display the default dashboard
+function init(){
+
+}
+// Update plots/charts via change from HTML onchange Event Attribute
+function optionChanged(value){
+
+}
+
+
+// Create the Plots
+function plots(){
+
+}
+
+// Add the Demographics Information 
+function demographics(){
+    
+}
