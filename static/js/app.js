47b92ff1ef4c9b24161def45f94e1d287b89d717
Tameka Kuar <tamekakuar17@gmail.com>
2020-09-02 12:48:02 -0400

    update code

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,41 @@
 // Display the default dashboard
 function init(){
+    // Select dropdown menus
+    var dropdown = d3.select('#selDataset');
 
+    // Request data and return promise
+    d3.json("data/samples.json").then(function(data){
+
+        // Print data
+        console.log(data);
+
+        // Append sample name to dropdown
+        data.names.forEach(function(name){
+            dropdown.append('option').text(name).property('value');
+        });
+
+        // Generate and display plots and demographics info corresponding to first ID
+        plots(data.names[0]);
+        demographics(data.names[0]);
+    });
 }
-// Update plots/charts via change from HTML onchange Event Attribute
-function optionChanged(value){
-
+// Update plots/charts via change from HTML onchange Event Attribute i.e. event listener 
+function optionChanged(id){
+    // Update plot based on value
+    plots(id);
+    // Update demographics based on value
+    demographics(id);
 }
 
 
 // Create the Plots
-function plots(){
+function plots(id){
 
 }
 
-// Add the Demographics Information 
-function demographics(){
-    
+// Add  information to Demographic Info panel
+function demographics(id){
+
 }
+
+init();
